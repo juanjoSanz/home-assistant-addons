7cca4414bf124925369926b3b28f578ae97fb2b3
juanjoSanz <juanjo.sanzmartin@gmail.com>
2026-09-25 12:04:13 +0200

    twc fixes

---
+++ twc-manager/CHANGELOG.md
@@ -0,0 +1,2 @@
+## 1.4.betaX
+- Initial release, testing dynamic configuration
--- twc-manager/run.sh
+++ twc-manager/run.sh
@@ -23,13 +23,13 @@ if bashio::config.true 'serial.enabled'; then
 
     ## Change config file
     bashio::log.info "Setting USB SERIAL with $serial_dev and $serial_baud..."
-    sed -id "s|###USBPORT_GEN3###|$serial_dev|" /etc/twcmanager/config.json
-    sed -id "s|###USBPORT_BAUDRATE###|$serial_baud|" /etc/twcmanager/config.json
+    sed -i "s|###USBPORT_GEN3###|$serial_dev|" /etc/twcmanager/config.json
+    sed -i "s|###USBPORT_BAUDRATE###|$serial_baud|" /etc/twcmanager/config.json
     bashio::log.info "Setting ###FUSEAMPS### with $fuse_amps..."
-    sed -id "s|###FUSEAMPS###|$fuse_amps|" /etc/twcmanager/config.json
-    sed -id "s|###PHASES###|$phases|" /etc/twcmanager/config.json
+    sed -i "s|###FUSEAMPS###|$fuse_amps|" /etc/twcmanager/config.json
+    sed -i "s|###PHASES###|$phases|" /etc/twcmanager/config.json
     bashio::log.info "Setting ###IP_GEN3### with $twc_address..."
-    sed -id "s|###IP_GEN3###|$twc_address|" /etc/twcmanager/config.json
+    sed -i "s|###IP_GEN3###|$twc_address|" /etc/twcmanager/config.json
 
 fi
 
@@ -46,16 +46,16 @@ if bashio::config.true 'mqtt.enabled'; then
 
     ## Change config file to use MQTT variables
     bashio::log.info "Setting MQTT broker ..."
-    sed -id "s|###MQTTENABLE###|true|" /etc/twcmanager/config.json
-    sed -id "s|###MQTTBROKER###|$mqtt_broker|" /etc/twcmanager/config.json
-    sed -id "s|###MQTTPORT###|$mqtt_port|" /etc/twcmanager/config.json
-    sed -id "s|###MQTTUSER###|$mqtt_user|" /etc/twcmanager/config.json
-    sed -id "s|###MQTTPASS###|$mqtt_pass|" /etc/twcmanager/config.json
-    sed -id "s|###MQTT_CONSUMPTION_TOPIC###|$mqtt_consumption_topic|" /etc/twcmanager/config.json
-    sed -id "s|###MQTT_GENERATION_TOPIC###|$mqtt_generation_topic|" /etc/twcmanager/config.json
+    sed -i "s|###MQTTENABLE###|true|" /etc/twcmanager/config.json
+    sed -i "s|###MQTTBROKER###|$mqtt_broker|" /etc/twcmanager/config.json
+    sed -i "s|###MQTTPORT###|$mqtt_port|" /etc/twcmanager/config.json
+    sed -i "s|###MQTTUSER###|$mqtt_user|" /etc/twcmanager/config.json
+    sed -i "s|###MQTTPASS###|$mqtt_pass|" /etc/twcmanager/config.json
+    sed -i "s|###MQTT_CONSUMPTION_TOPIC###|$mqtt_consumption_topic|" /etc/twcmanager/config.json
+    sed -i "s|###MQTT_GENERATION_TOPIC###|$mqtt_generation_topic|" /etc/twcmanager/config.json
 else
     bashio::log.info "MQTT disabled"
-    sed -id "s|###MQTTENABLE###|false|" /etc/twcmanager/config.json
+    sed -i "s|###MQTTENABLE###|false|" /etc/twcmanager/config.json
 fi
 
 
